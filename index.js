dc4db28599d895e8427197213dcea7a53b30edb0
SamSaysHecc <80711125+Sameony@users.noreply.github.com>
2021-09-03 10:13:51 +0000

    Completed ex 5 and 6

--- index.js
+++ index.js
@@ -10,15 +10,21 @@
 
 // // ----- end ex04 
 
-//ex-05
+//ex-05,06
+var score= 0;
 var readlineSync = require('readline-sync');
 var guess = parseInt(readlineSync.question("Guess my age "));
 var myAge = 20;
 if(guess===myAge)
 {
+  score++;
   console.log("Correct!");
+  console.log("Your score = "+score);
 }
 else
 {
+  score--;
   console.log("Wrong...");
+  console.log("Your score = "+score);
+  
 }
