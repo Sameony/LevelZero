ec45c2652dc4edc8e27e7fb155250c0649ce60d5
SamSaysHecc <80711125+Sameony@users.noreply.github.com>
2021-09-03 10:03:39 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+                //ex-4
+//ex 01
+console.log("Sameer");
+//ex 02
+var readlineSync = require('readline-sync');
+var myName = readlineSync.question("I require thy name! ");
+console.log("Hello! " +myName);
+//ex 03
+console.log("WELCOME "+myName+"!");
+
+// ----- end ex04 
+
