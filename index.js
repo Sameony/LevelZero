4689b27234ce5fbd7c7231bf8797185055a72bf7
SamSaysHecc <80711125+Sameony@users.noreply.github.com>
2021-09-03 10:20:53 +0000

    Exercise 8 quiz

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // //ex 01
 // console.log("Sameer");
 // //ex 02
-// var readlineSync = require('readline-sync');
+var readlineSync = require('readline-sync');
 // var myName = readlineSync.question("I require thy name! ");
 // console.log("Hello! " +myName);
 // //ex 03
@@ -10,21 +10,59 @@
 
 // // ----- end ex04 
 
-//ex-05,06
-var score= 0;
-var readlineSync = require('readline-sync');
-var guess = parseInt(readlineSync.question("Guess my age "));
-var myAge = 20;
-if(guess===myAge)
-{
-  score++;
-  console.log("Correct!");
-  console.log("Your score = "+score);
-}
-else
-{
-  score--;
-  console.log("Wrong...");
-  console.log("Your score = "+score);
+
+
+// //ex-05,06
+// var score= 0;
+// var readlineSync = require('readline-sync');
+// var guess = parseInt(readlineSync.question("Guess my age "));
+// var myAge = 20;
+// if(guess===myAge)
+// {
+//   score++;
+//   console.log("Correct!");
+//   console.log("Your score = "+score);
+// }
+// else
+// {
+//   score--;
+//   console.log("Wrong...");
+//   console.log("Your score = "+score);
   
+// }
+
+
+
+// //ex-07
+
+// function add(num1, num2)
+// {
+//   return num1+num2;
+// }
+// console.log(add(2,4));
+
+//ex 08
+Result=0;
+function quiz(Ques,Ans)
+{
+  if(Ques===Ans)
+  {
+    Result++;
+    console.log("You are correct!");
+    console.log("Your score is now: "+Result);
+  }
+  else
+  {
+    Result--;
+    console.log("You are Wrong!");
+    console.log("Your score is now: "+Result);
+  }
 }
+
+var Ques = readlineSync.question("Guess my mood! ");
+var Ans = "sad";
+(quiz(Ques,Ans));
+
+var Ques = readlineSync.question("Where do i live? ");
+var Ans  = "delhi";
+quiz(Ques,Ans);
