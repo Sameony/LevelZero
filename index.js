84dcea0196aa1e5e5ecb3a51b60688898dbaf37c
SamSaysHecc <80711125+Sameony@users.noreply.github.com>
2021-09-03 10:11:41 +0000

    added ParseInt for the variable of age

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-                //ex-4
-//ex 01
-console.log("Sameer");
-//ex 02
+//                 //ex-4
+// //ex 01
+// console.log("Sameer");
+// //ex 02
+// var readlineSync = require('readline-sync');
+// var myName = readlineSync.question("I require thy name! ");
+// console.log("Hello! " +myName);
+// //ex 03
+// console.log("WELCOME "+myName+"!");
+
+// // ----- end ex04 
+
+//ex-05
 var readlineSync = require('readline-sync');
-var myName = readlineSync.question("I require thy name! ");
-console.log("Hello! " +myName);
-//ex 03
-console.log("WELCOME "+myName+"!");
-
-// ----- end ex04 
-
+var guess = parseInt(readlineSync.question("Guess my age "));
+var myAge = 20;
+if(guess===myAge)
+{
+  console.log("Correct!");
+}
+else
+{
+  console.log("Wrong...");
+}
